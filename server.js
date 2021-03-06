27bfeed440d1507dc05a83c11e1fa27acaa5c95f
tuannv <nguyentuan060196@gmail.com>
2021-03-06 14:08:49 +0700

    ud

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 'use strict';
 let PAGE_ACCESS_TOKEN = "EAAMfpc4yQYkBACHtTgsDV2ocjbbRLqU1YsbIldG9mqLEZAJIV8IJD5ZAhfdcq9NcbixEe7eFcIZAbMkqY2UAjx19SuTQhVk6CTJMZC6n5tuXTSV78Yv3j7f9ZBt8QL51WGAOqfPgUPGDcrHJF3qKMRUO9LlLqoDr8jeZA2luwqxgZDZD";
-// Your verify token. Should be a random string.
-let VERIFY_TOKEN = "TUANNGUYENPX";
-  
-  
+
   
 // Imports dependencies and set up http server
 const
@@ -17,33 +14,32 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
-  // Parse the request body from the POST
-  let body = req.body;
+	  let VERIFY_TOKEN = "TUANNGUYENPX";
+	  // Parse the request body from the POST
+	  let body = req.body;
 
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
+	  // Check the webhook event is from a Page subscription
+	  if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+		// Iterate over each entry - there may be multiple if batched
+		body.entry.forEach(function(entry) {
 
-  // Gets the body of the webhook event
-  let webhook_event = entry.messaging[0];
-  console.log(webhook_event);
+	  // Gets the body of the webhook event
+	  let webhook_event = entry.messaging[0];
+	  console.log(webhook_event);
 
+	  // Get the sender PSID
+	  let sender_psid = webhook_event.sender.id;
+	  console.log('Sender PSID: ' + sender_psid);
 
-  // Get the sender PSID
-  let sender_psid = webhook_event.sender.id;
-  console.log('Sender PSID: ' + sender_psid);
-
-  // Check if the event is a message or postback and
-  // pass the event to the appropriate handler function
-  if (webhook_event.message) {
-    handleMessage(sender_psid, webhook_event.message);        
-  } else if (webhook_event.postback) {
-    handlePostback(sender_psid, webhook_event.postback);
-  }
-  
-});
+	  // Check if the event is a message or postback and
+	  // pass the event to the appropriate handler function
+	  if (webhook_event.message) {
+		handleMessage(sender_psid, webhook_event.message);        
+	  } else if (webhook_event.postback) {
+		handlePostback(sender_psid, webhook_event.postback);
+	  }
+	});
 
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
